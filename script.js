444c1160dea08dd42a9f42adad5089d8ab8f0ff4
evelynoliveiramello-alt <evelyn.oliveira.mello@escola.pr.gov.br>
2026-06-11 15:51:55 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("entrar").onclick=function(){
+  
+document.getElementById("home").style.display="none";
+  
+document.getElementById("site").style.display="block";
+  
+windw.scrollTo(0,0);
+  
+
+}
